1d1322659e2629e713a127886e5c8809880d7c73
zongxiongchen <czxczf@gmail.com>
2018-10-10 20:03:11 +0200

    add training dataset for play neural network

--- generate_operator_plot.py
+++ generate_operator_plot.py
@@ -7,6 +7,7 @@ sess = tf.Session()
 
 
 def generate_play_operator(weight=1.0, width=1.0, method="integer"):
+    print("Processing method: {}, weeight: {}, width: {}".format(method, weight, width))
     if method == "integer":
         _inputs = np.random.uniform(low=-10, high=10, size=500)
     elif method == "float":
@@ -20,6 +21,12 @@ def generate_play_operator(weight=1.0, width=1.0, method="integer"):
     outputs = pi_cell(inputs)
     inputs_res = sess.run(inputs)
     outputs_res = sess.run(outputs)
+
+    res = np.vstack([inputs_res, outputs_res]).T
+    np.savetxt("./training-data/operators/{}-{}-{}.csv".format(method, weight, width),
+                res, fmt="%.3f.", delimiter=",", header="x,p")
+
+
     plt.figure()
     plt.scatter(inputs_res[1:], outputs_res[1:])
     fname = "./pics/operators/{}-{}-{}.pdf".format(method, weight, width)
