421ecf3822c865d5757999a93145fe35dc403421
zongxiongchen <czxczf@gmail.com>
2018-10-16 22:34:06 +0200

    add example pavel needs

--- operator_test.py
+++ operator_test.py
@@ -17,14 +17,21 @@ def generate_play_operator(weight=1.0, width=1.0, method="integer"):
     elif method == "float":
         _inputs = np.random.uniform(low=-10, high=10, size=points)
     elif method == "sin":
-        _inputs1 = 10 * np.sin(np.linspace(-np.pi, np.pi, points))
-        _inputs2 = 5 * np.sin(np.linspace(-np.pi, np.pi, points))
-        _inputs = np.hstack([_inputs1, _inputs2])
+        # _inputs1 = 10 * np.sin(np.linspace(-np.pi, np.pi, points))
+        # _inputs2 = 5 * np.sin(np.linspace(-np.pi, np.pi, points))
+        _inputs1 = np.sin(np.linspace(-np.pi, np.pi, points))
+        _inputs2 = 3 * np.sin(np.linspace(-np.pi, np.pi, points))
+        _inputs3 = 1.2 * np.sin(np.linspace(-np.pi, np.pi, points))
+        _inputs = np.hstack([_inputs1, _inputs2, _inputs3])
 
     inputs = tf.constant(_inputs, dtype=tf.float32)
 
     pi_cell = PlayCell(weight=weight, width=width, debug=True)
     outputs = pi_cell(inputs, 0)
+
+    init = tf.global_variables_initializer()
+    sess.run(init)
+
     inputs_res = sess.run(inputs)
     outputs_res = sess.run(outputs)
 
@@ -73,17 +80,18 @@ def fit(inputs, outputs, width, method, true_weight):
 
     init = tf.global_variables_initializer()
     sess.run(init)
-    epochs = 30
+    epochs = 500
     for i in range(epochs):
         _, loss_value = sess.run((opt, loss))
         print("epoch", i, "loss:", loss_value, ", weight: ", pi_cell.kernel.eval(session=sess))
 
-    with open("./training-data/operators/trained-{}-{}-{}.txt".format(method, true_weight, width), "w") as f:
+    with open("./training-data/operators/pavel-trained-{}-{}-{}.txt".format(method, true_weight, width), "w") as f:
         print("true weight: {}, estimated_weight: {}".format(true_weight, pi_cell.kernel.eval(session=sess)))
+        f.write("epochs: {}, loss: {}\n".format(epochs, loss_value))
         f.write("true_weight: {}, estimated_weight: {}".format(true_weight, pi_cell.kernel.eval(session=sess)))
 
     outputs_ = pi_cell(inputs, state)
-    fname = "./training-data/operators/pred-{}-{}-{}.csv".format(method, true_weight, width)
+    fname = "./training-data/operators/pavel-pred-{}-{}-{}.csv".format(method, true_weight, width)
     save(inputs, outputs_.eval(session=sess), fname)
 
     # import ipdb; ipdb.set_trace()
@@ -95,28 +103,31 @@ if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == "save":
         save_anim = True
 
-    methods = ["integer", "float"]
-    widths = [1, 2, 3, 4, 5, 5.5, 6.5, 7, 9, 10, 12, 20, 100]
+    # methods = ["integer", "float"]
+    # widths = [1, 2, 3, 4, 5, 5.5, 6.5, 7, 9, 10, 12, 20, 100]
+    # weights = [1.0, 2.0, 3.0, 4.5]
+    methods = ["sin"]
+    widths = [1]
     weights = [1.0, 2.0, 3.0, 4.5]
 
-    for method in methods:
-        for weight in weights:
-            for width in widths:
-                inputs, outputs = generate_play_operator(weight, width, method)
-                fname = "./training-data/operators/{}-{}-{}.csv".format(method, weight, width)
-                save(inputs, outputs, fname)
-                # if save_anim is True:
-                #     fname = "./pics/operators/{}-{}-{}.gif".format(method, weight, width)
-                #     xlim = [np.min(inputs) - 1, np.max(inputs) + 1]
-                #     ylim = [np.min(outputs) - 1, np.max(outputs) + 1]
-                #     save_animation(inputs, outputs, fname, xlim, ylim)
-                #     print("Finished ", fname)
-                # else:
-                #     plt.show()
+    # for method in methods:
+    #     for weight in weights:
+    #         for width in widths:
+    #             inputs, outputs = generate_play_operator(weight, width, method)
+    #             fname = "./training-data/operators/pavel-{}-{}-{}.csv".format(method, weight, width)
+    #             save(inputs, outputs, fname)
+    #             if save_anim is True:
+    #                 fname = "./pics/operators/pavel-{}-{}-{}.gif".format(method, weight, width)
+    #                 xlim = [np.min(inputs) - 1, np.max(inputs) + 1]
+    #                 ylim = [np.min(outputs) - 1, np.max(outputs) + 1]
+    #                 save_animation(inputs, outputs, fname, xlim, ylim)
+    #                 print("Finished ", fname)
+    #             else:
+    #                 plt.show()
 
     for method in methods:
         for weight in weights:
             for width in widths:
-                fname = "./training-data/operators/{}-{}-{}.csv".format(method, weight, width)
+                fname = "./training-data/operators/pavel-{}-{}-{}.csv".format(method, weight, width)
                 inputs, outputs = load(fname)
                 fit(inputs, outputs, width, method, weight)
