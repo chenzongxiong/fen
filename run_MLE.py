347a170d2decae487c3526dceab8f9b351f529c0
zongxiong.chen <czxczf@gmail.com>
2019-05-21 20:01:13 +0200

    tanh, 20 plays, 20 units epochs: 200

--- run_MLE.py
+++ run_MLE.py
@@ -27,7 +27,7 @@ def fit(inputs,
         weights_name='model.h5',
         loss_name='mse'):
 
-    epochs = 500
+    epochs = 200
     # steps_per_epoch = batch_size
 
     start = time.time()
@@ -236,8 +236,8 @@ if __name__ == "__main__":
     activation = 'tanh'
     activation = None
     ############################## predicitons #############################
-    __nb_plays__ = 20
-    __units__ = 20
+    __nb_plays__ = 100
+    __units__ = 100
     # __nb_plays__ = 50
     # __units__ = 50
 
