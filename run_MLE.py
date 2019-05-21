b0139e6543871a1f0809445cd25803a3803cef17
zongxiong.chen <czxczf@gmail.com>
2019-05-21 17:31:22 +0200

    relu, epochs 200, 20 units 20 plays

--- run_MLE.py
+++ run_MLE.py
@@ -27,7 +27,7 @@ def fit(inputs,
         weights_name='model.h5',
         loss_name='mse'):
 
-    epochs = 300
+    epochs = 500
     # steps_per_epoch = batch_size
 
     start = time.time()
@@ -244,7 +244,7 @@ if __name__ == "__main__":
     __state__ = 0
     __activation__ = 'tanh'
     __activation__ = 'relu'
-    __activation__ = None
+    # __activation__ = None
     __mu__ = 0
     __sigma__ = 70
     # __sigma__ = 5
