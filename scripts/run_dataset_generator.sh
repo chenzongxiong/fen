91e3763d30d238de814c94ab4453d7ee743fb7cc
Zongxiong Chen <czxczf@gmail.com>
2019-12-17 19:28:34 +0100

    regenerate dataset

--- scripts/run_dataset_generator.sh
+++ scripts/run_dataset_generator.sh
@@ -1,72 +1,72 @@
-# # tanh
-# python dataset_generator.py --model-noise --points 100 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation tanh
-# python dataset_generator.py --model-noise --points 1000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation tanh
-# python dataset_generator.py --model-noise --points 10000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation tanh
-# python dataset_generator.py --model-noise --points 100000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation tanh
-
-# python dataset_generator.py --model-noise --points 100 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation tanh
-# python dataset_generator.py --model-noise --points 1000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation tanh
-# python dataset_generator.py --model-noise --points 10000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation tanh
-# python dataset_generator.py --model-noise --points 100000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation tanh
-
-# python dataset_generator.py --model-noise --points 100 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation tanh
-# python dataset_generator.py --model-noise --points 1000 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation tanh
-# python dataset_generator.py --model-noise --points 10000 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation tanh
-# python dataset_generator.py --model-noise --points 100000 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation tanh
-
-# python dataset_generator.py --model-noise --points 100 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation tanh
-# python dataset_generator.py --model-noise --points 1000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation tanh
-# python dataset_generator.py --model-noise --points 10000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation tanh
-# python dataset_generator.py --model-noise --points 100000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation tanh
-
-# # elu
-# python dataset_generator.py --model-noise --points 100 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation elu
-# python dataset_generator.py --model-noise --points 1000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation elu
-# python dataset_generator.py --model-noise --points 10000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation elu
-# python dataset_generator.py --model-noise --points 100000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation elu
-
-# python dataset_generator.py --model-noise --points 100 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation elu
-# python dataset_generator.py --model-noise --points 1000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation elu
-# python dataset_generator.py --model-noise --points 10000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation elu
-# python dataset_generator.py --model-noise --points 100000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation elu
-
-# python dataset_generator.py --model-noise --points 100 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation elu
-# python dataset_generator.py --model-noise --points 1000 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation elu
-# python dataset_generator.py --model-noise --points 10000 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation elu
-# python dataset_generator.py --model-noise --points 100000 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation elu
-
-# python dataset_generator.py --model-noise --points 100 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation elu
-# python dataset_generator.py --model-noise --points 1000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation elu
-# python dataset_generator.py --model-noise --points 10000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation elu
-# python dataset_generator.py --model-noise --points 100000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation elu
-
-# # relu
-# python dataset_generator.py --model-noise --points 100 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation relu
-# python dataset_generator.py --model-noise --points 1000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation relu
-# python dataset_generator.py --model-noise --points 10000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation relu
-# python dataset_generator.py --model-noise --points 100000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation relu
-
-# python dataset_generator.py --model-noise --points 100 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation relu
-# python dataset_generator.py --model-noise --points 1000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation relu
-# python dataset_generator.py --model-noise --points 10000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation relu
-# python dataset_generator.py --model-noise --points 100000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation relu
-
-# python dataset_generator.py --model-noise --points 100 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation relu
-# python dataset_generator.py --model-noise --points 1000 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation relu
-# python dataset_generator.py --model-noise --points 10000 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation relu
-# python dataset_generator.py --model-noise --points 100000 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation relu
-
-# python dataset_generator.py --model-noise --points 100 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation relu
-# python dataset_generator.py --model-noise --points 1000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation relu
-# python dataset_generator.py --model-noise --points 10000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation relu
-# python dataset_generator.py --model-noise --points 100000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation relu
-
-
 # tanh
-python dataset_generator.py --diff-weights --model-noise --points 100 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation tanh
-python dataset_generator.py --diff-weights --model-noise --points 1000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation tanh
-python dataset_generator.py --diff-weights --model-noise --points 10000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation tanh
-python dataset_generator.py --diff-weights --model-noise --points 100000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation tanh
+python dataset_generator.py --model-noise --points 100 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation tanh
+python dataset_generator.py --model-noise --points 1000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation tanh
+python dataset_generator.py --model-noise --points 10000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation tanh
+python dataset_generator.py --model-noise --points 100000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation tanh
+
+python dataset_generator.py --model-noise --points 100 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation tanh
+python dataset_generator.py --model-noise --points 1000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation tanh
+python dataset_generator.py --model-noise --points 10000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation tanh
+python dataset_generator.py --model-noise --points 100000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation tanh
+
+python dataset_generator.py --model-noise --points 100 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation tanh
+python dataset_generator.py --model-noise --points 1000 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation tanh
+python dataset_generator.py --model-noise --points 10000 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation tanh
+python dataset_generator.py --model-noise --points 100000 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation tanh
+
+python dataset_generator.py --model-noise --points 100 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation tanh
+python dataset_generator.py --model-noise --points 1000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation tanh
+python dataset_generator.py --model-noise --points 10000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation tanh
+python dataset_generator.py --model-noise --points 100000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation tanh
+
+# elu
+python dataset_generator.py --model-noise --points 100 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation elu
+python dataset_generator.py --model-noise --points 1000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation elu
+python dataset_generator.py --model-noise --points 10000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation elu
+python dataset_generator.py --model-noise --points 100000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation elu
+
+python dataset_generator.py --model-noise --points 100 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation elu
+python dataset_generator.py --model-noise --points 1000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation elu
+python dataset_generator.py --model-noise --points 10000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation elu
+python dataset_generator.py --model-noise --points 100000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation elu
+
+python dataset_generator.py --model-noise --points 100 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation elu
+python dataset_generator.py --model-noise --points 1000 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation elu
+python dataset_generator.py --model-noise --points 10000 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation elu
+python dataset_generator.py --model-noise --points 100000 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation elu
+
+python dataset_generator.py --model-noise --points 100 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation elu
+python dataset_generator.py --model-noise --points 1000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation elu
+python dataset_generator.py --model-noise --points 10000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation elu
+python dataset_generator.py --model-noise --points 100000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation elu
+
+# relu
+python dataset_generator.py --model-noise --points 100 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation relu
+python dataset_generator.py --model-noise --points 1000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation relu
+python dataset_generator.py --model-noise --points 10000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation relu
+python dataset_generator.py --model-noise --points 100000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation relu
+
+python dataset_generator.py --model-noise --points 100 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation relu
+python dataset_generator.py --model-noise --points 1000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation relu
+python dataset_generator.py --model-noise --points 10000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation relu
+python dataset_generator.py --model-noise --points 100000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation relu
+
+python dataset_generator.py --model-noise --points 100 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation relu
+python dataset_generator.py --model-noise --points 1000 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation relu
+python dataset_generator.py --model-noise --points 10000 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation relu
+python dataset_generator.py --model-noise --points 100000 --nb_plays 100 --units 100 --mu 0 --sigma 1 --activation relu
+
+python dataset_generator.py --model-noise --points 100 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation relu
+python dataset_generator.py --model-noise --points 1000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation relu
+python dataset_generator.py --model-noise --points 10000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation relu
+python dataset_generator.py --model-noise --points 100000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation relu
+
+
+# # tanh
+# python dataset_generator.py --diff-weights --model-noise --points 100 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation tanh
+# python dataset_generator.py --diff-weights --model-noise --points 1000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation tanh
+# python dataset_generator.py --diff-weights --model-noise --points 10000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation tanh
+# python dataset_generator.py --diff-weights --model-noise --points 100000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation tanh
 
 python dataset_generator.py --diff-weights --model-noise --points 100 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation tanh
 python dataset_generator.py --diff-weights --model-noise --points 1000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation tanh
@@ -84,10 +84,10 @@ python dataset_generator.py --diff-weights --model-noise --points 10000 --nb_pla
 python dataset_generator.py --diff-weights --model-noise --points 100000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation tanh
 
 # elu
-python dataset_generator.py --diff-weights --model-noise --points 100 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation elu
-python dataset_generator.py --diff-weights --model-noise --points 1000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation elu
-python dataset_generator.py --diff-weights --model-noise --points 10000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation elu
-python dataset_generator.py --diff-weights --model-noise --points 100000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation elu
+# python dataset_generator.py --diff-weights --model-noise --points 100 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation elu
+# python dataset_generator.py --diff-weights --model-noise --points 1000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation elu
+# python dataset_generator.py --diff-weights --model-noise --points 10000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation elu
+# python dataset_generator.py --diff-weights --model-noise --points 100000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation elu
 
 python dataset_generator.py --diff-weights --model-noise --points 100 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation elu
 python dataset_generator.py --diff-weights --model-noise --points 1000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation elu
@@ -105,10 +105,10 @@ python dataset_generator.py --diff-weights --model-noise --points 10000 --nb_pla
 python dataset_generator.py --diff-weights --model-noise --points 100000 --nb_plays 500 --units 100 --mu 0 --sigma 1 --activation elu
 
 # relu
-python dataset_generator.py --diff-weights --model-noise --points 100 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation relu
-python dataset_generator.py --diff-weights --model-noise --points 1000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation relu
-python dataset_generator.py --diff-weights --model-noise --points 10000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation relu
-python dataset_generator.py --diff-weights --model-noise --points 100000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation relu
+# python dataset_generator.py --diff-weights --model-noise --points 100 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation relu
+# python dataset_generator.py --diff-weights --model-noise --points 1000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation relu
+# python dataset_generator.py --diff-weights --model-noise --points 10000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation relu
+# python dataset_generator.py --diff-weights --model-noise --points 100000 --nb_plays 1 --units 1 --mu 0 --sigma 1 --activation relu
 
 python dataset_generator.py --diff-weights --model-noise --points 100 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation relu
 python dataset_generator.py --diff-weights --model-noise --points 1000 --nb_plays 50 --units 50 --mu 0 --sigma 1 --activation relu
